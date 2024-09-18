5d2561b6bc0fd93346365a03e4cb8f459d9c2101
Victor Hugo <victorhugof.lisboa@gmail.com>
2024-09-18 01:49:57 -0300

    codegen: Adiciona função de alocação de variáveis

--- src/codegen.c
+++ src/codegen.c
@@ -1,4 +1,6 @@
 #include "codegen.h"
+#include "tables.h"
+#include "string.h"
 
 const char *read_proc = \
 	"read: li a7, 5\n"
@@ -13,3 +15,29 @@ const char *write_proc = \
 char data[0xFFFF];
 char text[0xFFFF] = "call main\nli a7, 10\necall\n";
 
+void allocate_variables(st_cell **symbol_table) {
+	/* Popula .data com variáveis */
+	st_cell *ptr = *symbol_table;
+	char space_num[5];
+	
+	while(!ptr->next) {
+		st_cell *current = ptr->next;
+		type_t type = current->sym_type;
+		if(type == VOID_T) {
+			continue;
+		}
+		if(type == INT_T) {
+			strcpy(space_num, "4");
+ 		}
+		else {
+			int len = current->len;
+			sprintf(space_num, "%d", len*4);
+		}
+
+		char line[40];
+		char id[20] = current->name;
+		sprintf(line, "%s: .space %s\n", id, space_num); // line = id + : + .space: + space_num
+		
+		strcat(data, line);
+	}
+}
